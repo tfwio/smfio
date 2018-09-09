04475357708f70d9ee9d8a87402fed7cf77571e2
tfwio <tfwroble@gmail.com>
2018-09-09 14:55:46 -0500

    comments

--- .src/MidiReaderExt.cs
+++ .src/MidiReaderExt.cs
@@ -17,6 +17,11 @@ namespace SMFIOViewer
 				yield return i;
 		}
 
+		/// <summary>
+		/// This just gathers simple "MTrk {id+1}" string keys into an index.
+		/// </summary>
+		/// <param name="parser">the MIDI parser</param>
+		/// <returns></returns>
 		static public IEnumerable<KeyValuePair<int, string>> GetMidiTrackNameDictionary(this IMidiParser parser)
 		{
 			foreach (int i in parser.EnumerateTrackIndex()) {
@@ -25,6 +30,12 @@ namespace SMFIOViewer
 			}
 		}
 
+		/// <summary>
+		/// Uses Linq to find/filter all channel-specific events from parser.MidiDataList.
+		/// </summary>
+		/// <param name="parser"></param>
+		/// <param name="trackid"></param>
+		/// <returns></returns>
 		static public IEnumerable<MidiMessage> MidiTrackDistinctChannels(this IMidiParser parser, int trackid)
 		{
       return parser.MidiDataList[trackid].Distinct(ChannelComparer);
