9993178d336ffc27da1ebf07a41272e830d4d0a4
tfwio <tfwroble@gmail.com>
2018-09-15 15:29:47 -0500

    add a build script to clean out all bins

---
+++ Build-CleanALL.bat
@@ -0,0 +1,7 @@
+@echo off
+REM set msbuild_path=C:\Program Files (x86)\msbuild\14.0\bin
+set msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+set PATH=%PATH%;%msbuild_path%
+msbuild ".sln\\smfio.sln" "/t:smfio:Clean" "/p:Platform=Any CPU;Configuration=Release"
+msbuild ".sln\\smfio.sln" "/t:smfio:Clean" "/p:Platform=Any CPU;Configuration=Debug"
+
