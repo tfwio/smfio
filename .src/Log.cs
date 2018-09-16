099dfd9771c77f98a091e3fa0960d0973f07c488
tfwio <tfwroble@gmail.com>
2018-09-15 19:14:23 -0500

    use message box if there is no CONSOLE #define during compilation (this is going to need another look)

--- .src/Log.cs
+++ .src/Log.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Windows.Forms;
 namespace on.smfio
 {
   public class Log
   {
     public static void ErrorMessage(string format, params object[] arg)
     {
-      #if CONSOLE
+      #if !CONSOLE
       MessageBox.Show(string.Format(format, arg), "Press OK to continue", MessageBoxButtons.OK);
       #else
       Console.WriteLine("Unknown Message: {0}", arg);
