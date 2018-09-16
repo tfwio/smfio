5ad224bf8769683e891f652b1021091feb55c4ae
tfwio <tfwroble@gmail.com>
2018-09-15 19:13:29 -0500

    no more stack overflow (oops)

--- .src/TimeUtil.cs
+++ .src/TimeUtil.cs
@@ -5,17 +5,13 @@ namespace on.smfio
   public class TimeUtil
   {
     const string DefaultMBTFormat = "{0:##,###,###,000}:{1:0#}:{2:00#}";
-    public static string GetMBT(ulong pulse, int division, bool plusOne = true, string filter = DefaultMBTFormat)
-    {
-      return GetMBT(pulse, division, plusOne, filter);
-    }
     public static string GetMBT(long pulse, int division, bool plusOne = true, string filter = DefaultMBTFormat)
     {
       int plusmode = plusOne ? 1 : 0;
       double value = (double)pulse;
       var M = Convert.ToInt32(Math.Floor(value / (division * 4.0)) + plusmode);
       var B = Convert.ToInt32((Math.Floor(value / division) % 4) + plusmode);
-      var T = pulse % division;
+      var T = (int)(pulse % division);
       return string.Format(filter, M, B, T);
     }
     public static double GetSeconds(int division, double tempo, long pulse, double sec = 0.0)
