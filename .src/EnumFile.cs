1764cff03013c39f30d8b384cb5b361655e2da2c
tfwio <tfwroble@gmail.com>
2018-10-01 06:52:35 -0500

    embed external text-file './ext' info(s).
    Now we're embedding the external files.
    
    We can still supply the files at the same ./ext/[file] location to over-ride the default characteristics.

--- .src/EnumFile.cs
+++ .src/EnumFile.cs
@@ -5,8 +5,30 @@
 
 namespace on.smfio
 {
+  static class x0 {
+    //"on.smfio.ext.cc.map"
+    static public string[] GetEmbeddedResourceString(this System.Reflection.Assembly asm, string resID, char separator = (char)0x0A)
+    {
+      string[] result = null;
+      using (var strm = typeof(EnumFile).Assembly.GetManifestResourceStream(resID))
+        using (var reader = new System.IO.StreamReader(strm))
+      {
+        result = reader.ReadToEnd().Split(separator);
+      }
+      return result;
+    }
+    static public string[] GetLines(this string enumText, char split=(char)0x0A)
+    {
+      var abs = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().FullName, enumText));
+      if (System.IO.File.Exists(abs))
+        return System.IO.File.ReadAllText(enumText, System.Text.Encoding.UTF8).Split(split);
+      return null;
+      
+    }
+  }
   public class EnumFile
   {
+    public EnumFile(){}
     string[] data_map;
     
     public string this[int index] { get { return (index > (Length - 1)) ? null : data_map[index]; } }
@@ -14,7 +36,9 @@ namespace on.smfio
     public int Length { get { return data_map.Length; } }
     
     public EnumFile(string file) { Load(file); }
-    
+    static public EnumFile CMAP { get { return new EnumFile{data_map="ext/cc.map".GetLines()   ?? typeof(EnumFile).Assembly.GetEmbeddedResourceString("on.smf.ext.cc.map") }; } }
+    static public EnumFile DMAP { get { return new EnumFile{data_map="ext/dk.map".GetLines()   ?? typeof(EnumFile).Assembly.GetEmbeddedResourceString("on.smf.ext.dk.map") }; } }
+    static public EnumFile IMAP { get { return new EnumFile{data_map="ext/inst.map".GetLines() ?? typeof(EnumFile).Assembly.GetEmbeddedResourceString("on.smf.ext.inst.map") }; } }
     void Load(string file)
     {
       if (!System.IO.File.Exists(file))
