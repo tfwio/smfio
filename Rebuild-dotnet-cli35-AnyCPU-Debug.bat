6e7214601e9bbdce84744c252e4347e17e0ccc7a
tfwio <tfwroble@gmail.com>
2018-10-14 14:32:23 -0500

    add multi-threading flag + rebuild (debug)

--- Rebuild-dotnet-cli35-AnyCPU-Debug.bat
+++ Rebuild-dotnet-cli35-AnyCPU-Debug.bat
@@ -3,4 +3,4 @@
 REM set msbuild_path=C:\Program Files (x86)\msbuild\14.0\bin
 set msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 set PATH=%PATH%;%msbuild_path%
-msbuild /m ".sln\\smfio.sln" "/t:smfio_view" "/p:Platform=Any CPU;Configuration=Release"
+msbuild /m ".sln\\smfio.sln" "/t:smfio_view:Rebuild" "/p:Platform=Any CPU;Configuration=Debug"
