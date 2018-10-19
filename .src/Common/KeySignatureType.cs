3bb4dff3c1ca0f3351853c277e8b52087dd142e2
tfwio <tfwroble@gmail.com>
2018-10-19 13:25:13 -0500

    correct KeySignatureType names/descriptions

--- .src/Common/KeySignatureType.cs
+++ .src/Common/KeySignatureType.cs
@@ -24,47 +24,47 @@ namespace on.smfio.Common
   public enum KeySignatureType : byte
 	{
     /// <summary>C&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    CFlat = 0xF0, // -7
+    [System.ComponentModel.Description("C\u266D (7 Flats)")]
+    C_Flat = 0xF0, // -7
     /// <summary>G&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    GFlat = 0xFA, // -6
+    [System.ComponentModel.Description("G\u266D (6 Flats)")]
+    G_Flat = 0xFA, // -6
     /// <summary>D&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    DFlat = 0xFB, // -5
+    [System.ComponentModel.Description("D\u266D (5 Flats)")]
+    D_Flat = 0xFB, // -5
     /// <summary>A&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    AFlat = 0xFC, // -4
+    [System.ComponentModel.Description("A\u266D (4 Flats)")]
+    A_Flat = 0xFC, // -4
     /// <summary>E&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    EFlat = 0xFD, // -3
+    [System.ComponentModel.Description("E\u266D (3 Flats)")]
+    E_Flat = 0xFD, // -3
     /// <summary>B&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    BFlat = 0xFE, // -2
+    [System.ComponentModel.Description("B\u266D (2 Flats)")]
+    B_Flat = 0xFE, // -2
     /// <summary>F&#x266D;</summary>
-    [System.ComponentModel.Description("C\u266D")]
-    FFlat = 0xFF, // -1
-    [System.ComponentModel.Description("C\u266D")]
-    C = 0,        // 0
+    [System.ComponentModel.Description("F (1 Flat)")]
+    F = 0xFF, // -1
     [System.ComponentModel.Description("C")]
-    GSharp = 1,   // 1
+    C = 0,        // 0
+    [System.ComponentModel.Description("G (1 Sharp)")]
+    G = 1,   // 1
     /// <summary>D#</summary>
-    [System.ComponentModel.Description("D#")]
-    DSharp = 2,   // 2
+    [System.ComponentModel.Description("D (2 Sharps)")]
+    D = 2,   // 2
     /// <summary>A#</summary>
-    [System.ComponentModel.Description("A#")]
-    ASharp = 3,   // 3
+    [System.ComponentModel.Description("A (3 Sharps)")]
+    A = 3,   // 3
     /// <summary>E#</summary>
-    [System.ComponentModel.Description("E#")]
-    ESharp = 4,   // 4
+    [System.ComponentModel.Description("E (4 Sharps)")]
+    E = 4,   // 4
     /// <summary>B#</summary>
-    [System.ComponentModel.Description("B#")]
-    BSharp =5,    // 5
+    [System.ComponentModel.Description("B (5 Sharps)")]
+    B =5,    // 5
     /// <summary>F#</summary>
-    [System.ComponentModel.Description("F#")]
-    FSharp = 6,   // 6
+    [System.ComponentModel.Description("F# (6 Sharps)")]
+    F_Sharp = 6,   // 6
     /// <summary>C#</summary>
-    [System.ComponentModel.Description("C#")]
-    CSharp = 7,   // 7
+    [System.ComponentModel.Description("C# (7 Sharps)")]
+    C_Sharp = 7,   // 7
 	}
 }
