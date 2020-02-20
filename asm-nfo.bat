9bd0c5f33f31af50d9b8312bd9dddfd9f3d899f9
tfwio <tfwroble@gmail.com>
2020-02-20 09:14:12 -0600

    let python v3.7 (and 3.6) generate our ./Properties/AssemblyInfo.cs file
    i.e. %AppDataLocal%\\Programs\\Python[v]\\python.exe

--- asm-nfo.bat
+++ asm-nfo.bat
@@ -1,4 +1,13 @@
-#! cmd.exe /c
-pushd %~dp0
-"%LOCALAPPDATA%\Programs\Python\Python36\python.exe" "get-sha"
-popd
+@ECHO OFF
+PUSHD %~dp0
+  CALL :DOTHIS Python36
+  CALL :DOTHIS Python37
+POPD
+PAUSE
+GOTO:EOF
+
+
+:DOTHIS
+  if EXIST "%LOCALAPPDATA%\Programs\Python\%~1" "%LOCALAPPDATA%\Programs\Python\%~1\python.exe" "get-sha"
+  GOTO:EOF
+
