a4540b3ac76227df938d3732388d5cf24eebc7b2
tfwio <tfwroble@gmail.com>
2020-02-25 19:15:46 -0600

    remove/ignore *.snk + clean asm-nfo.gen script

--- asm-nfo.bat
+++ asm-nfo.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
-PUSHD %~dp0
+
+PUSHD %~dp0 > nul
   CALL :DOTHIS Python36
   CALL :DOTHIS Python37
-POPD
-PAUSE
+POPD > nul
+::PAUSE
 GOTO:EOF
 
-
 :DOTHIS
-  if EXIST "%LOCALAPPDATA%\Programs\Python\%~1" "%LOCALAPPDATA%\Programs\Python\%~1\python.exe" "get-sha"
+  if EXIST "%LOCALAPPDATA%\Programs\Python\%~1" "%LOCALAPPDATA%\Programs\Python\%~1\python.exe" "get-sha" > nul
   GOTO:EOF
 
