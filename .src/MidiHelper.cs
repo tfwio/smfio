f6ec9c719610e81906e408dc30c9a5553bb8cedd
tfwio <tfwroble@gmail.com>
2020-03-01 05:09:03 -0600

    `OctaveMacro()` key-index zero '0' should yields "C -1" now; not "C 0"
    See "./doc/smfio-tech-ref.pdf"

--- .src/MidiHelper.cs
+++ .src/MidiHelper.cs
@@ -23,19 +23,11 @@ namespace System
 {
   static public class MidiHelper
   {
+    const int TOP = 128;
     static public string[] OctaveMacro()
     {
-      string[] ax = new string [ 128 ];
-      for (int i = 0; i<=127; i++) ax[i] = string.Format("{0}{1}", keys[i % 12],Convert.ToInt32(i/12));
-      return ax;
-    }
-    static public string[] OctaveMacro(int lo, int hi)
-    {
-      string[] ax = new string [ hi-lo ];
-      int octave = 0;
-      for (int i = octave; i < hi; i+=12)  {
-        ax[i] = string.Format("{0}{1}",keys[i % 12],Convert.ToInt32(i/12));
-      }
+      string[] ax = new string [ TOP ];
+      for (int i = 0; i < TOP; i++) ax[i] = $"{keys[i % 12]}{Convert.ToInt32(i / 12) - 1,3}";
       return ax;
     }
     public static string[] Byze { get => OctaveMacro(); }
