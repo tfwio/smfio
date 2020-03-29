6ef7170a7e853c6d37c0e6af09fef48da525522d
tfwio <tfwroble@gmail.com>
2020-03-29 05:13:00 -0500

    update build config

--- Build-dotnet-cli35-AnyCPU-Release.bat
+++ Build-dotnet-cli35-AnyCPU-Release.bat
@@ -1,6 +1,23 @@
-#! cmd.exe /c
 @echo off
+SET SNKSIZE=4096
+SET SNKN=smfio
+SET PLAT=AnyCPU
+SET CONF=Debug
+SET CNF=%PLAT%-%CONF%
+
+IF NOT EXIST smfio\.src\smfio.snk (
+  pushd smfio\.src > nul
+    echo - Generate Strong Name Key Pair ^(%SNKN%.snk^)
+    sn -k %SNKSIZE% %SNKN%.snk > nul
+    echo - Generate Public Key ^(%SNKN%-public.snk^)
+    sn -p %SNKN%.snk %SNKN%-public.snk sha512 > nul
+    echo - Public Key Information
+    sn -tp %SNKN%-public.snk
+  popd > nul
+)
+
 REM set msbuild_path=C:\Program Files (x86)\msbuild\14.0\bin
 set msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 set PATH=%PATH%;%msbuild_path%
 msbuild /m ".sln\\smfio.sln" "/t:smfio_view" "/p:Platform=Any CPU;Configuration=Release"
+pause
--- Build-dotnet-cli35-Clean_ALL.bat
+++ Build-dotnet-cli35-Clean_ALL.bat
@@ -1,4 +1,3 @@
-#! cmd.exe /c
 @echo off
 REM set msbuild_path=C:\Program Files (x86)\msbuild\14.0\bin
 set msbuild_path=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
@@ -9,3 +8,4 @@ msbuild /m ".sln\\smfio.sln" "/t:smfio:Clean" "/p:Platform=Win32;Configuration=R
 msbuild /m ".sln\\smfio.sln" "/t:smfio:Clean" "/p:Platform=Win32;Configuration=Debug"
 msbuild /m ".sln\\smfio.sln" "/t:smfio_view:Clean" "/p:Platform=Any CPU;Configuration=Release"
 msbuild /m ".sln\\smfio.sln" "/t:smfio_view:Clean" "/p:Platform=Any CPU;Configuration=Debug"
+pause
